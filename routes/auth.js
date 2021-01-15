4d60a8ee9a0ead9731047a037a3e2c63e4763aa6
NarekKarapetyan97 <narek.karapetyan97@gmail.com>
2021-01-15 02:04:40 -0500

    first

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const { Router } = require('express')
 const passport = require('passport')
+const redis = require('redis')
 
 const rout = Router()
+
+const publisher = redis.createClient()
+const subscriber = redis.createClient()
 
 rout.post(
     '/login',
@@ -12,5 +16,19 @@ rout.post(
         res.sendStatus(200)
     }
 )
+
+rout.get('/publisher', (req, res) => {
+    publisher.publish('user-notify', JSON.stringify(req.user))
+    res.send('Publishing an Event using Redis')
+})
+
+subscriber.on('message', (channel, message) => {
+    console.log('Received data :' + message)
+})
+
+rout.get('/subscriber', (req, res) => {
+    subscriber.subscribe('user-notify')
+    res.send('Subscriber One')
+})
 
 module.exports = rout
